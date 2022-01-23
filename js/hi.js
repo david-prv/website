ffb3e01d87af54884ec1e22db3e3f03a63752ad5
David <66866223+david-prv@users.noreply.github.com>
2022-01-23 11:07:26 +0100

    Create hi.js

---
+++ js/hi.js
@@ -0,0 +1,29 @@
+// Define hellos
+let hellos = ["Zdravo", "Zdravo", "Ahoj", "Hallo", "Hei", "Salut", "Ciao", "Sveiki", "Olà", "Merhaba", "Konnichiwa", "Hej", "Shalom", "Jambo", "Hoi", "Salve"];
+let final_hello = "Hey";
+
+// Delayed loop
+let iterations = 0;
+
+let x = setInterval(function() {
+
+    // Start timed fade-out
+    if(iterations == hellos.length - 6) document.getElementById("hello").classList.add("fade-out");
+
+    // Loop through all hellos
+    if(iterations < hellos.length) {
+        document.getElementById("hello").innerText = hellos[iterations] + ",";
+        iterations += 1;
+    } else {
+        // Stop loop
+        clearInterval(x);
+        
+        // Fade-in
+        setTimeout(function() {
+            document.getElementById("hello").innerText = final_hello + ",";
+            document.getElementById("hello").classList.remove("fade-out");
+            document.getElementById("hello").classList.add("fade-in");
+        }, 700);
+    }
+
+}, 200);
