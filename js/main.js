09d59f6ae843684b61b5f0c1f587d0b8e12a098b
David <66866223+david-prv@users.noreply.github.com>
2022-02-21 13:24:21 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -61,18 +61,19 @@ async function togglePrivacy() {
 // Function that actually inserts the imprint
 function showImprint() {
     tempHtml = document.getElementById("dynamic").innerHTML;
-    document.getElementById("dynamic").innerHTML = `<h2>Imprint</h2>
+    document.getElementById("dynamic").innerHTML = `<h1>Imprint</h1>
 
-<strong>Information according to &sect; 5 TMG</strong>
+<h2>Information according to &sect; 5 TMG</h2>
 <p>David Dewes<br />
 Schw&auml;hnselstr. 5<br />
 66125 Saarbr&uuml;cken</p>
 
-<strong>Contact</strong>
+<h2>Contact</h2>
 <p>E-Mail: <span id="7DlKjh2+8_j(j4d5FF6">[<a style="cursor: pointer;font-weight:bolder;text-decoration: underline;" onClick="document.getElementById('7DlKjh2+8_j(j4d5FF6').innerHTML = atob('aW1wcmludEBkYXZpZC1kZXdlcy5kZQ==')">Reveal</a>]</span></p>
 
 <p>Source: <a href="https://www.e-recht24.de/impressum-generator.html">https://www.e-recht24.de/impressum-generator.html</a><br>
 Permanent Link: <a href="${imprintUrl}">${imprintUrl}</a></p>
+<p>Background Animation was created by <a href="https://codepen.io/LeonGr">LeonGr</a>.</p>
     <a class="float-right list-nav" onclick="toggleImprint()">< Go back</a>`;
     
 }
@@ -91,4 +92,115 @@ function checkWindow() {
         document.getElementById("privacy").innerText = "Privacy";
         document.getElementById("imprint").innerText = "Imprint";
     }
+}
+
+// =============================================================
+// Background animation made by: https://codepen.io/LeonGr
+// =============================================================
+
+let canvas, ctx;
+
+var stars = [], // Array that contains the stars
+    FPS = 60, // Frames per second
+    x = window.innerWidth/20, // Number of stars
+    mouse = {
+      x: 0,
+      y: 0
+    };  // mouse location
+
+// Draw the scene
+
+function draw() {
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+  
+  ctx.globalCompositeOperation = "lighter";
+  
+  for (var i = 0, x = stars.length; i < x; i++) {
+    var s = stars[i];
+  
+    ctx.fillStyle = "#fff";
+    ctx.beginPath();
+    ctx.arc(s.x, s.y, s.radius, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.fillStyle = 'black';
+    ctx.stroke();
+  }
+  
+  ctx.beginPath();
+  for (var i = 0, x = stars.length; i < x; i++) {
+    var starI = stars[i];
+    ctx.moveTo(starI.x,starI.y); 
+    if(distance(mouse, starI) < 150) ctx.lineTo(mouse.x, mouse.y);
+    for (var j = 0, x = stars.length; j < x; j++) {
+      var starII = stars[j];
+      if(distance(starI, starII) < 150) {
+        //ctx.globalAlpha = (1 / 150 * distance(starI, starII).toFixed(1));
+        ctx.lineTo(starII.x,starII.y); 
+      }
+    }
+  }
+  ctx.lineWidth = 0.05;
+  ctx.strokeStyle = 'white';
+  ctx.stroke();
+}
+
+function distance( point1, point2 ){
+  var xs = 0;
+  var ys = 0;
+ 
+  xs = point2.x - point1.x;
+  xs = xs * xs;
+ 
+  ys = point2.y - point1.y;
+  ys = ys * ys;
+ 
+  return Math.sqrt( xs + ys );
+}
+
+// Update star locations
+
+function update() {
+  for (var i = 0, x = stars.length; i < x; i++) {
+    var s = stars[i];
+  
+    s.x += s.vx / FPS;
+    s.y += s.vy / FPS;
+    
+    if (s.x < 0 || s.x > canvas.width) s.vx = -s.vx;
+    if (s.y < 0 || s.y > canvas.height) s.vy = -s.vy;
+  }
+}
+
+// Update and draw
+
+function tick() {
+  draw();
+  update();
+  requestAnimationFrame(tick);
+}
+
+function init() {
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext('2d');
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    canvas.addEventListener('mousemove', function(e){
+        mouse.x = e.clientX;
+        mouse.y = e.clientY;
+    });
+
+    // Push stars to array
+
+    for (var i = 0; i < x; i++) {
+        stars.push({
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+        radius: Math.random() * 1 + 1,
+        vx: Math.floor(Math.random() * 50) - 25,
+        vy: Math.floor(Math.random() * 50) - 25
+        });
+    }
+
+    tick();
 }
